057d899eeb6c44e918e64eedbdb87cd2c2ca2423
Владимир Александрович Райгель <vraygel@mail.ru>
2024-04-02 13:31:47 +0500

    коррекция стилей списка терминов

--- utils/checkingDate.js
+++ utils/checkingDate.js
@@ -62,7 +62,7 @@ async function checkingDate(req, res, next) {
 						if (user.alerts.telegramm) {
 							const message = `
 							<a href="https://frlpt.site/study/repetition/"><b>Пора повторить термины!</b></a>
-							\n
+							
 							Настроить оповещения можно в профиле: <a href="https://frlpt.site/user/profile/"><b>Профиль</b></a>
 							`
 							if (userChatId != '') {
