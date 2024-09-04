adcc9f55284eaa22e8d3218b0435d1fd1d8279cd
Владимир Александрович Райгель <vraygel@mail.ru>
2024-09-04 10:27:09 +0500

    Revert "удалил не рабочий скрипт и стили"
    This reverts commit 8167bd0e12653ca9fb8fe9dba82fda86e665d88f.

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,17 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+	let hamburger = document.querySelector('.hamburger')
+
+	hamburger.addEventListener('click', () => {
+		toggleMenu(hamburger)
+	})
+	
+	function toggleMenu(hamburger) {
+		hamburger.classList.toggle('is-active');
+	}
+	
+
 // перенести в приложение
 
 let glossary = document.querySelectorAll('.glossary');
